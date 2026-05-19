911a94932d6ca7abdca8eb881133c4a6d1722979
Juan Jose Urbano Perdomo <urbanoperdomoj@gmail.com>
2026-05-18 23:32:34 -0500

    cambios en el settings

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--$-8*-t!y0=74y=mg-%oj_)^&(gtk!jr=h^#n0xt=(-mf1xazs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
